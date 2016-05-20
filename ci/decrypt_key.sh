3209388d813343b6aef936a7a04adafc28c841c5
Roman3349 <ondracek.roman@centrum.cz>
2016-05-20 20:57:41 +0200

    Fixed bug in generation of documentation
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

--- ci/decrypt_key.sh
+++ ci/decrypt_key.sh
@@ -1,3 +1,4 @@
-openssl aes-256-cbc -K $encrypted_4d23fe3cd75c_key -iv $encrypted_4d23fe3cd75c_iv -in travis-ci_rsa.enc -out ci/travis-ci_rsa -d
+#!/bin/bash -e
+openssl aes-256-cbc -K $encrypted_4d23fe3cd75c_key -iv $encrypted_4d23fe3cd75c_iv -in ci/travis-ci_rsa.enc -out ci/travis-ci_rsa -d
 chmod 0600 ci/travis-ci_rsa
 cp ci/travis-ci_rsa ~/.ssh/id_rsa
