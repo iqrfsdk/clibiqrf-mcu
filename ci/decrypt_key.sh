e0dae15adba9d44bcebc9458b1354b49ca00c806
Roman3349 <ondracek.roman@centrum.cz>
2016-05-20 20:52:21 +0200

    Changed private key to push generated documentation
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

--- ci/decrypt_key.sh
+++ ci/decrypt_key.sh
@@ -1,3 +1,3 @@
-openssl aes-256-cbc -K $encrypted_f7e980bb1803_key -iv $encrypted_f7e980bb1803_iv -in ci/travis-ci_rsa.enc -out ci/travis-ci_rsa -d
+openssl aes-256-cbc -K $encrypted_4d23fe3cd75c_key -iv $encrypted_4d23fe3cd75c_iv -in travis-ci_rsa.enc -out ci/travis-ci_rsa -d
 chmod 0600 ci/travis-ci_rsa
 cp ci/travis-ci_rsa ~/.ssh/id_rsa
