1dbf805dc8cb483ee8d556a00f867b06a73919f7
Documentation Builder <travis@microrisc.com>
2016-05-21 20:02:28 +0000

    Automated documentation build for changeset f3b6aa8d050c2e88ffd7e28b94b59303437a4acb.

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['check',['check',['../classIQRFCRC.html#ae655e6b17bc1f54af20641fd78509630',1,'IQRFCRC']]],
   ['checkcrcs',['CheckCRCS',['../iqrf__library_8cpp.html#a4e7aa2e03c41feac0b744633e62ff679',1,'iqrf_library.cpp']]],
   ['communication_5fmode',['COMMUNICATION_MODE',['../iqrf__library_8h.html#a3345fd5832d738cf1ca862fe7b6c9759',1,'iqrf_library.h']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['check',['check',['../classIQRFCRC.html#ae655e6b17bc1f54af20641fd78509630',1,'IQRFCRC']]],
   ['checkcrcs',['CheckCRCS',['../iqrf__library_8cpp.html#a4e7aa2e03c41feac0b744633e62ff679',1,'iqrf_library.cpp']]]
 ];
