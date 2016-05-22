f15ed5a0d3ac996314ab8f4735e4b212fae71aa8
Documentation Builder <travis@microrisc.com>
2016-05-22 05:13:02 +0000

    Automated documentation build for changeset 2b83769bd410096baf9c0f509b9a4472d7323e9d.

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['calculate',['calculate',['../classIQRFCRC.html#a12ab28c36ae21da9a6bbfc6a6fee96ae',1,'IQRFCRC']]],
   ['check',['check',['../classIQRFCRC.html#ae655e6b17bc1f54af20641fd78509630',1,'IQRFCRC']]],
   ['checkcrcs',['CheckCRCS',['../iqrf__library_8cpp.html#a4e7aa2e03c41feac0b744633e62ff679',1,'iqrf_library.cpp']]],
   ['communication_5fmode',['COMMUNICATION_MODE',['../iqrf__library_8h.html#a3345fd5832d738cf1ca862fe7b6c9759',1,'iqrf_library.h']]]
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['calculate',['calculate',['../classIQRFCRC.html#a12ab28c36ae21da9a6bbfc6a6fee96ae',1,'IQRFCRC']]],
   ['check',['check',['../classIQRFCRC.html#ae655e6b17bc1f54af20641fd78509630',1,'IQRFCRC']]],
   ['checkcrcs',['CheckCRCS',['../iqrf__library_8cpp.html#a4e7aa2e03c41feac0b744633e62ff679',1,'iqrf_library.cpp']]]
 ];
