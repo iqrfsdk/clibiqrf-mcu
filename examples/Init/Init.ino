23bcaa6ef80cdac09e12cadadf583fa89bf14717
Roman3349 <ondracek.roman@centrum.cz>
2016-05-28 15:49:36 +0200

    Refactored example
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

--- examples/Init/Init.ino
+++ examples/Init/Init.ino
@@ -22,25 +22,26 @@
 #define USER_TIMER_PERIOD 5000
 
 // LOCAL PROTOTYPES
-void MyIqrfRxHandler(void);
-void MyIqrfTxHandler(uint8_t txPktId, uint8_t txPktResult);
-void AppTimerHandler(void);
-void cb_timer1ms(void);
+void rxHandler();
+void txHandler(uint8_t packetId, uint8_t packetResult);
+void msTimerCallback();
 
 // GLOBAL VARIABLES
-// App data
+
+/**
+ * App data structure
+ */
 typedef struct {
-  uint8_t myIqrfRxBuf[IQ_PKT_SIZE];
-  uint8_t *myIqrfTxBuf;
-  uint8_t testPktId;
-  volatile uint16_t appTimer;
-  volatile uint8_t appTimerAck;
-} app_vars_t;
-app_vars_t app_vars;
+	uint8_t rxBuffer[IQ_PKT_SIZE];
+	uint8_t *txBuffer;
+	uint8_t packetId;
+	volatile uint16_t timer;
+	volatile bool timerAck;
+} appVarsStruct;
+appVarsStruct appVars;
 
 // Const data
 const uint8_t testBuffer[64] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64};
-const uint8_t ledPin = 13;
 
 // INSTANCES
 // IQRFTR instance
@@ -50,89 +51,82 @@ IQRFTR* iqrfTr = new IQRFTR;
  * Init peripherals
  */
 void setup() {
-  // User LED
-  pinMode(ledPin, OUTPUT);
-  // Up - PC
-  Serial.begin(9600);
-  // Down - IQRF
-  IQRF_Init(MyIqrfRxHandler, MyIqrfTxHandler);
-  // Info - TR
-  switch (IQRF_GetModuleType()) {
-    case iqrfTr->types::TR_52D:
-      Serial.println("Module type: TR-52D");
-      break;
-    case iqrfTr->types::TR_72D:
-      Serial.println("Module type: TR-72D");
-      break;
-    default:
-      Serial.println("Module type: UNKNOWN");
-      break;
-  }
-  MsTimer2::set(1, cb_timer1ms);
-  MsTimer2::start();
-  // Clear variables
-  memset(&app_vars, 0, sizeof(app_vars_t));
-  app_vars.appTimer = USER_TIMER_PERIOD;
-  // Done here
-  Serial.println("Peripherals and IQRF init done");
+	// User LED
+	pinMode(13, OUTPUT);
+	// Up - PC
+	Serial.begin(9600);
+	// Down - IQRF
+	IQRF_Init(rxHandler, txHandler);
+	// Info - TR
+	switch (IQRF_GetModuleType()) {
+		case iqrfTr->types::TR_52D:
+			Serial.println("Module type: TR-52D");
+			break;
+		case iqrfTr->types::TR_72D:
+			Serial.println("Module type: TR-72D");
+			break;
+		default:
+			Serial.println("Module type: UNKNOWN");
+			break;
+	}
+	MsTimer2::set(1, msTimerCallback);
+	MsTimer2::start();
+	// Clear variables
+	memset(&appVars, 0, sizeof (appVarsStruct));
+	appVars.timer = USER_TIMER_PERIOD;
+	// Done here
+	Serial.println("Peripherals and IQRF init done");
 }
 
 /**
  * Main loop
  */
 void loop() {
-  // TR module SPI comunication driver
-  IQRF_Driver();
-  // Test send data every 5s
-  if (app_vars.appTimerAck) {
-    // Allocate memory for Tx packet
-    app_vars.myIqrfTxBuf = (uint8_t *) malloc(sizeof(testBuffer));
-    if (app_vars.myIqrfTxBuf != NULL) {
-      // Copy data from test to IQRF TX packet
-      memcpy(app_vars.myIqrfTxBuf, (uint8_t *) &testBuffer, sizeof(testBuffer));
-      // Send data and unallocate data buffer
-      app_vars.testPktId = IQRF_SendData(app_vars.myIqrfTxBuf, sizeof(testBuffer), 1);
-    }
-    app_vars.appTimerAck = false;
-  }
+	// TR module SPI comunication driver
+	IQRF_Driver();
+	// Test send data every 5s
+	if (appVars.timerAck) {
+		// Allocate memory for Tx packet
+		appVars.txBuffer = (uint8_t *) malloc(sizeof (testBuffer));
+		if (appVars.txBuffer != NULL) {
+			// Copy data from test to IQRF TX packet
+			memcpy(appVars.txBuffer, (uint8_t *) & testBuffer, sizeof (testBuffer));
+			// Send data and unallocate data buffer
+			appVars.packetId = IQRF_SendData(appVars.txBuffer, sizeof (testBuffer), 1);
+		}
+		appVars.timerAck = false;
+	}
 }
 
 /**
  * 1ms timer callback
  */
-void cb_timer1ms() {
-  // app timer, call handler
-  if (app_vars.appTimer) {
-    if ((--app_vars.appTimer) == 0) {
-      AppTimerHandler();
-      app_vars.appTimer = USER_TIMER_PERIOD;
-    }
-  }
-}
-
-/**
- * User timer handler
- */
-void AppTimerHandler() {
-  app_vars.appTimerAck = true;
+void msTimerCallback() {
+	// app timer, call handler
+	if (appVars.timer) {
+		if ((--appVars.timer) == 0) {
+			appVars.timerAck = true;
+			appVars.timer = USER_TIMER_PERIOD;
+		}
+	}
 }
 
 /**
  * IQRF RX callback
  */
-void MyIqrfRxHandler() {
-  // Read and print received data
-  IQRF_GetRxData(app_vars.myIqrfRxBuf, IQRF_GetRxDataSize());
-  Serial.print("IQRF receive done: ");
-  Serial.write(app_vars.myIqrfRxBuf, IQRF_GetRxDataSize());
-  Serial.println();
+void rxHandler() {
+	// Read and print received data
+	IQRF_GetRxData(appVars.rxBuffer, IQRF_GetRxDataSize());
+	Serial.print("IQRF receive done: ");
+	Serial.write(appVars.rxBuffer, IQRF_GetRxDataSize());
+	Serial.println();
 }
 
 /**
  * IQRF TX callback
- * @param txPktId Paket ID
- * @param txPktResult Paket writing result
+ * @param packetId Packet ID
+ * @param packetResult Packet writing result
  */
-void MyIqrfTxHandler(uint8_t txPktId, uint8_t txPktResult) {
-  Serial.println("IQRF send done");
+void txHandler(uint8_t packetId, uint8_t packetResult) {
+	Serial.println("IQRF send done");
 }
